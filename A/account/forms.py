425e8c6a818428aa03052245afd8fd40e540ecc6
sajad <sajad.alimoradi@outlook.com>
2022-10-17 08:20:01 +0330

    Social Network

---
+++ A/account/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from .models import Profile
+
+class UserRegisterationForm(forms.Form):
+    username=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    email=forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    password1=forms.CharField(label='Password',widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'your password'}))    
+    password2=forms.CharField(label='Confirm Password',widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Confirm password'}))
+
+
+    def clean_email(self):
+        email=self.cleaned_data['email']
+        user=User.objects.filter(email=email).exists()
+        if user:
+            raise ValidationError('this email is already exists')
+        return email
+
+    def clean(self):
+        cd = super().clean()
+        p1 = cd.get('password1')
+        p2 = cd.get('password2')
+
+        if p1 and p2 and p1 != p2:
+            raise ValidationError('Password Not Match')
+
+
+
+class UserLoginForm(forms.Form):
+    username=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password=forms.CharField(label='Password',widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'your password'}))  
+
+
+class EditUserForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = Profile
+        fields = ('age','bio')      
